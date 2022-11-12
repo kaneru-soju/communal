16861010f1e7eff085128fb35e1eaffebfe95b70
kaneru <danielck@vt.edu>
2022-11-12 11:49:52 -0500

    cleaned up pages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,16 @@
-import { Component } from 'react';
 import EnterCard from '../components/EnterCard';
 
 import '../styles/Home.scss';
 
-class Home extends Component {
-    render() {
-        return (
+function Home() {
+    return (
         <div>
             <div className="stars small"></div>
             <div className="stars medium"></div>
             <div className="stars large"></div>
             <EnterCard />
         </div>
-        );
-    }
+    );
 }
 
 export default Home;
--- src/pages/Watch.jsx
+++ src/pages/Watch.jsx
@@ -1,13 +1,9 @@
-import { Component } from 'react';
-
-class Watch extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Watch</h1>
-            </div>
-        );
-    }
+function Watch() {
+    return (
+        <div>
+            <h1>Watch</h1>
+        </div>
+    );
 }
 
 export default Watch;
