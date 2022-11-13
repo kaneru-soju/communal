92faab6df60da401309d8d2f7890c67a7050ec11
kaneru <danielck@vt.edu>
2022-11-12 19:55:47 -0500

    webpack fixes

--- src/pages/Watch.jsx
+++ src/pages/Watch.jsx
@@ -1,69 +1,25 @@
-import '../styles/Watch.css'
+import WebTorrent from 'webtorrent';
 
-import ReactPlayer from 'react-player'
-import 'react-chatbox-component/dist/style.css';
-import {ChatBox} from 'react-chatbox-component';
-import { textAlign } from '@mui/system';
-import { red } from '@mui/material/colors';
-
-
-let messages = [
-    {
-      "text": "Moyai",
-      "id": "1",
-      "sender": {
-        "name": "Aniruthan",
-        "uid": "ani",
-        "avatar": "http://s3.amazonaws.com/pix.iemoji.com/images/emoji/apple/ios-12/256/moai.png",
-      },
-    },
-    {
-        "text": "Moyai",
-      "id": "1",
-      "sender": {
-        "name": "Sid",
-        "uid": "ani",
-        "avatar": "http://s3.amazonaws.com/pix.iemoji.com/images/emoji/apple/ios-12/256/moai.png",
-      },
-    },
-    {
-        "text": "Moyai",
-      "id": "1",
-      "sender": {
-        "name": "Kaneru",
-        "uid": "ani",
-        "avatar": "http://s3.amazonaws.com/pix.iemoji.com/images/emoji/apple/ios-12/256/moai.png",
-      },
-    }
-  ]
-
-function Watch() {
-    return (
-        <div className='video-chat-container'>
-            <div className='video-player'>
-            <ReactPlayer url='videos/big_buck_bunny_720p_surround.mp4' />
-            </div>
-            <div className='chat-box'>
-                <h5 style={{textAlign: 'center', padding: '5px', backgroundColor: 'black', color: 'white'}}>Chat</h5>
-                <ChatBox
-            messages={messages}
-            onSubmit={(msg) => {
-                messages.push( {
-                    "text": msg,
-                "id": "1",
-                "sender": {
-                    "name": "Sid",
-                    "uid": "ani",
-                    "avatar": "http://s3.amazonaws.com/pix.iemoji.com/images/emoji/apple/ios-12/256/moai.png",
-                },
-                })
-            }}
-            />
-            </div>
-        </div>
+import '../styles/Watch.css';
 
         
-    );
+function Watch() {
+  const client = new WebTorrent();
+  console.log(client);
+  const torrentId = 'magnet:?xt=urn:btih:03c75389c96437f8608cd8f7d19c482b14fcd486&dn=%5BAnime%20Land%5D%20One%20Piece%201039%20%28WEBRip%201080p%20Hi10P%20AAC%29%20RAW%20%5B16CBC36C%5D.mp4&tr=http%3A%2F%2Fnyaa.tracker.wf%3A7777%2Fannounce&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Fexodus.desync.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce';
+
+  client.add(torrentId, function (torrent) {
+    // Torrents can contain many files. Let's use the .mp4 file
+    const file = torrent.files.find(function (file) {
+      return file.name.endsWith('.mp4')
+    })
+
+    // Display the file by adding it to the DOM. Supports video, audio, image, etc. files
+    file.appendTo('body')
+  })
+  return (
+      <div></div>
+  );
 }
 
 export default Watch;
